585fabcf995617a0a2b0da79baf5f137362156e2
grantkee <grant.d.kee@gmail.com>
2019-12-12 14:56:56 -0600

    added basic navigation bar, working on filling out equipment component. I'm using grid and paper from material-ui right now, but I'm not sure that's what I want to use. I need to review other options before I go too far down this rabbit hole.

---
+++ src/components/00-Navigation.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, 
+    Typography } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+    return (
+        <AppBar position="relative">
+            <Toolbar>
+                <IconButton color="inherit">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" style={{ flexGrow: "1" }}>
+                    Travis Medical
+                </Typography>
+                <ul className="nav-list">
+                    <li className="nav-list-item">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="nav-list-item">
+                        <Link to="/client">New Client</Link>
+                    </li>
+                </ul>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Navigation;
--- src/components/3-Equipment.js
+++ src/components/3-Equipment.js
@@ -1,2 +1,41 @@
 import React from 'react';
-//import { NavBar } from ./NavBar
+import {makeStyles} from '@material-ui/core/styles'
+import Navigation from './00-Navigation';
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 140,
+      width: 100,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  }));
+
+export default function Equipment() {
+
+
+
+    return (
+        <>
+        <Navigation/>
+            <Grid container
+                direction='row'
+                justify='center'
+                alignItems='center'
+                >
+                    <Grid item
+                        justify='space-around'
+                        border='black'
+                    >
+                        <Paper width='300' height='100'>Hiiii</Paper>
+                    </Grid>
+                </Grid>
+        </>
+    )
+}
