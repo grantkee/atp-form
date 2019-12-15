5c8fd711579801c98859f1711708c15d960d29dc
grantkee <grant.d.kee@gmail.com>
2019-12-15 13:58:03 -0600

    minor change to navigation component

--- src/components/00-Navigation.js
+++ src/components/00-Navigation.js
@@ -4,7 +4,7 @@ import { AppBar, Toolbar, IconButton,
 import MenuIcon from '@material-ui/icons/Menu'
 import { Link } from 'react-router-dom'
 
-const Navigation = () => {
+const Navigation = () => { 
     return (
         <AppBar position="relative">
             <Toolbar>
