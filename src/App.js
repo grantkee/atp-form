a69400021a7fa121248d5ec7c6b79fe655ca28c0
grantkee <grant.d.kee@gmail.com>
2020-01-12 11:11:36 -0600

    removed redundant index.js file. Added BrowserRouter and Provider to my actual index.js. working on setting up the backend.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Navigation from './components/Navigation'
-import './App.css'
-import Router from './Router'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import Navigation from './components/Navigation';
+import './App.css';
+import Router from './Router';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -34,9 +37,13 @@ const theme = createMuiTheme({
 console.log(theme)
 
 ReactDOM.render(
-    <MuiThemeProvider theme={theme}>
-        <App />
-    </MuiThemeProvider>, 
+    <BrowserRouter>
+        <Provider store={ store }>
+            <MuiThemeProvider theme={theme}>
+                <App />
+            </MuiThemeProvider>
+        </Provider>
+    </BrowserRouter>, 
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
