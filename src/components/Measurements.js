67378e9d4583755f5af80213efd0dc130a8135dd
grantkee <grant.d.kee@gmail.com>
2020-01-13 09:14:51 -0600

    removed proxy from package.json file to work on Measurements component. Img for measurements guide is not loading properly, and I have no idea why. Moving on to create table for now.

--- src/components/Measurements.js
+++ src/components/Measurements.js
@@ -6,6 +6,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
+//table imports
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+//TableContainer not found at /core/TableContainer???
+import TableContainer from '@material-ui/core';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
 const useStyles = makeStyles(theme => ({
     form: {
     width: '100%', // Fix IE 11 issue.
@@ -29,6 +40,9 @@ function Measurements( props ) {
     return(
         <form className={classes.form} noValidate>
             <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <img src="../data/measurement-guide.svg" alt="measurements" height="50px"/>
+                </Grid>
                 <Grid item xs={12} sm={6}>
                 <TextField
                     autoComplete="dx"
