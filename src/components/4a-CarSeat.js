c7672355de2d6aa2dea070e1c1635210b2e363a4
grantkee <grant.d.kee@gmail.com>
2020-01-18 10:32:21 -0600

    minor adjustment to CarSeat. Pushing to change computers.

--- src/components/4a-CarSeat.js
+++ src/components/4a-CarSeat.js
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 const CarSeat = ( props ) => {
     return (
         <>
-        <FunctionalAssessment />
+            <FunctionalAssessment />
         </>
     )
 }
